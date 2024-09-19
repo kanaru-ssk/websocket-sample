641c0220dd982aac45a8c8eddf420b2fd9223aad
kanaru-ssk <kanaru.ssk@gmail.com>
2024-09-19 11:20:33 +0900

    api-gatewayにlogger追加

--- api-gateway/src/index.ts
+++ api-gateway/src/index.ts
@@ -9,6 +9,24 @@ const collaborationWsProxy = createProxyMiddleware({
   ws: true,
   changeOrigin: true,
   xfwd: true,
+  logger: console,
+  on: {
+    proxyReq: (proxyReq, req, res) => {
+      console.log("proxyReq:proxyReq", proxyReq);
+      console.log("proxyReq:req", req);
+      console.log("proxyReq:res", res);
+    },
+    proxyRes: (proxyRes, req, res) => {
+      console.log("proxyRes:proxyReq", proxyRes);
+      console.log("proxyRes:req", req);
+      console.log("proxyRes:res", res);
+    },
+    error: (err, req, res) => {
+      console.log("error:proxyReq", err);
+      console.log("error:req", req);
+      console.log("error:res", res);
+    },
+  },
 });
 
 app.use("/api/collaboration", collaborationWsProxy);
